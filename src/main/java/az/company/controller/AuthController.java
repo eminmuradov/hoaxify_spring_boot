7816d035bba946b8fa356b796ccb99ee7bc9f16b
emin.muradov <eminmurad97@gmail.com>
2021-09-09 17:30:40 +0400

    09.09.2021

--- src/main/java/az/company/controller/AuthController.java
+++ src/main/java/az/company/controller/AuthController.java
@@ -1,19 +1,47 @@
 package az.company.controller;
 
 
+import az.company.model.User;
+import az.company.repository.UserRepository;
+import az.company.response.ApiError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Base64;
+
 @RestController
 public class AuthController {
-    private static final Logger logger= LoggerFactory.getLogger(AuthController.class);
 
+    @Autowired
+    UserRepository userRepository;
 
-    void handleAuthentication(@RequestHeader(name = "Authorization",required = false) String authorization){
-        if (authorization==null){
+    ResponseEntity<?> handleAuthentication(@RequestHeader(name = "Authorization", required = false) String authorization) {
 
+        if (authorization == null) {
+            ApiError error = new ApiError(401, "Unauthorized error", "/api/1.0/auth");
+
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
         }
+
+        String bas64encoded = authorization.split("Basic ")[1];
+        String decoded = new String(Base64.getDecoder().decode(bas64encoded));
+
+        String[] parts = decoded.split(":");
+        String username = parts[0];
+        String password = parts[1];
+
+        User inDb = userRepository.findByUsername(username);
+
+        if (inDb == null) {
+            ApiError error= new ApiError(401,"Unauthorized error","/api/1.0/auth");
+        }
+
+
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/az/company/response/ApiError.java
+++ src/main/java/az/company/response/ApiError.java
@@ -1,5 +1,6 @@
 package az.company.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -11,6 +12,7 @@ import java.util.Map;
 @Getter
 @Setter
 @ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiError {
 
 
